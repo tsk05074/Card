047ad0e21d5e87f4d0c7e1c4ffeb8bc6de24b6f4
tsk05074 <66294530+tsk05074@users.noreply.github.com>
2023-03-13 18:04:16 +0900

    1. Movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Movement moveGM;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
